b1fe1ca47c6a2ee637adbd16ad60acedf21fd162
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-10-09 16:40:15 +0700

    update details ajax

--- FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEM/MVCWebApp/Controllers/AjaxController.cs
+++ FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEM/MVCWebApp/Controllers/AjaxController.cs
@@ -3,6 +3,7 @@ using KoiOrderingSystem.Data.Models;
 using KoiOrderingSystem.Service.Base;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace MVCWebApp.Controllers
 {
@@ -35,6 +36,43 @@ namespace MVCWebApp.Controllers
                 }
             }
             return Json(new { message = "No data found", data = new List<Travel>() });
+        }
+
+        public IActionResult Details(Guid? id)
+        {
+            var model = new Travel { Id = id.Value }; // Tạo mô hình mới với ID
+            return View(model);
+        }
+
+        // GET: Ajax/GetDetailsDetails/5
+        public async Task<JsonResult> GetTravelDetails(Guid? id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Travels/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<Travel>(result.Data.ToString());
+                            return Json(new
+                            {
+                                success = true,
+                                data
+                            });
+                        }
+                    }
+                }
+            }
+            return Json(new
+            {
+                success = false,
+                message = "Travel not found."
+            });
         }
     }
 }
